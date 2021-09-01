9f1273a2d89021ccd173b5ac6521ea85da2fdfba
pangchao <pang.xchao@qq.com>
2021-09-01 17:04:31 +0800

    备份

--- mini-mvc/src/main/java/com/mini/core/mvc/MiniErrorExceptionHandler.java
+++ mini-mvc/src/main/java/com/mini/core/mvc/MiniErrorExceptionHandler.java
@@ -1,13 +1,12 @@
 package com.mini.core.mvc;
 
 import com.mini.core.mvc.validation.ValidateException;
+import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * 自定义错误处理类，防止自定义错误打印异常日志
@@ -23,8 +22,9 @@ public class MiniErrorExceptionHandler {
      * @param request  HttpServletRequest 对象
      * @param response HttpServletResponse 对象
      */
+    @SneakyThrows
     @ExceptionHandler({ValidateException.class})
-    public void validate(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void validate(ValidateException exception, HttpServletRequest request, HttpServletResponse response) {
         request.getRequestDispatcher("/error").forward(request, response);
     }
 }
